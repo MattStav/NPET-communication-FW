10835626b7c7433cae48315228a457230d7df9fb
MattStav <matej.stavinoha@volny.cz>
2026-08-07 23:21:19 +0200

    Added function to start measurement on NPEt

--- src/framework/NPET_dual.cpp
+++ src/framework/NPET_dual.cpp
@@ -1,12 +1,23 @@
 #include "NPET_dual.h"
 
 
+///
+/// Set measurement data format to binary on an NPET
+/// @param npet NPETComm instance to set the measured data format
 static void setMeasuredDataFormatToBinary(NPETComm &npet) {
     SPDLOG_DEBUG("Setting measured data format to binary for NPET");
     if (!npet.setMeasuredDataFormat(0)) {
         SPDLOG_ERROR(DATA_FORMAT_ERR);
         throw std::runtime_error(std::string(DATA_FORMAT_ERR));
     }
+}
+
+///
+/// Start a measurement on NPET using the supplied measurement context.
+/// @param npet NPETComm instance to start the measurement on
+/// @param meas_set The measurement context containing the settings for the measurement
+static void startMeasurement(NPETComm &npet, MeasContext const &meas_set) {
+    [[maybe_unused]] MeasReader const SESSION(npet, meas_set);
 }
 
 

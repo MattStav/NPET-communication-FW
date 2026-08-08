539d52a17014fe8e3141bc5fcfc67ecdd23cdc74
MattStav <matej.stavinoha@volny.cz>
2026-08-08 14:41:20 +0200

    Extracted some CLI messages to common structure

--- src/cli/helper_func.h
+++ src/cli/helper_func.h
@@ -12,6 +12,11 @@ constexpr std::string_view INVALID_COM_PORT = "Invalid COM port number";
 constexpr std::string_view FAILED_OPEN_COM_PORT = "Failed to open the COM port: {}";
 constexpr std::string_view FAILED_OPEN_COM_PORT_MAX_ATTEMPT = "Failed to open NPET communication after {} attempts";
 constexpr std::string_view INVALID_NUM = "Invalid input. Number out of allowed range";
+constexpr std::string_view PULSE_GEN_STOP_FAIL = "Failed to stop pulse generation";
+constexpr std::string_view FREQ_RESET_FAIL = "Failed to reset the pulse generation frequency";
+constexpr std::string_view BAUD_RATE_RESET_FAIL = "Failed to reset baud rate to default";
+constexpr std::string_view DATA_FORMAT_RESET_FAIL = "Failed to reset the data format";
+constexpr std::string_view TIME_CONST_FAILED_TO_CLEAR = "Failed to clear the time correction constant saved in NPET";
 
 int printManual();
 
